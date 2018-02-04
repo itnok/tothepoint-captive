cca65895d1750e85c7cb546b06edf27199767103
Simone Conti <s.conti@itnok.com>
2018-02-04 12:53:56 -0800

    [SETUP] Basic Netlify integration
    Signed-off-by: Simone Conti <s.conti@itnok.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,9 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Captive Portal for AdvancedTomato based on Nuxt and Netlify' }
     ],
+    script: [
+      { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
+    ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
